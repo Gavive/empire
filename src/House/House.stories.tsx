238843f13a683a225c29d99e99783518d55bb9be
Vishrut Reddi <vishrutreddi@gmail.com>
2020-07-25 00:35:26 -0700

    Add yarn.lock, rollup and storybook

---
+++ src/House/House.stories.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import House from "./House";
+
+export default {
+  title: "House"
+};
+
+export const Primary = () => <House />
--- src/House/House.tsx
+++ src/House/House.tsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./House.scss";
+import { State, Props } from "./House.types";
 
-export default class House extends Component {
+export default class House extends Component<Props, State> {
 
   public render(): React.ReactNode {
     return (
       <div className="house">
+        Hello World
       </div>
     );
   }
