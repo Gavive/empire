c4f545e165403f26bf50e3bcbf9738ad9380b2f3
Vishrut Reddi <vishrutreddi@gmail.com>
2020-07-24 18:32:08 -0700

    Add base files

---
+++ src/House/House.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./House.scss";
+
+export default class House extends Component {
+
+  public render(): React.ReactNode {
+    return (
+      <div className="house">
+      </div>
+    );
+  }
+}
