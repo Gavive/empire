95785da52244408a56b2bf878ac43daf6b89cc7d
Vishrut Reddi <vishrutreddi@gmail.com>
2020-08-16 00:07:05 -0700

    Add DropDown tests, Fix body functionality
    * Add dropdown improvements
    
    * Add dropdown tests

--- src/DropDown/DropDown.stories.tsx
+++ src/DropDown/DropDown.stories.tsx
@@ -3,7 +3,19 @@ import centered from '@storybook/addon-centered/react';
 import React from "react";
 
 import DropDown from './DropDown';
+import { DropDownOption } from './DropDown.types';
+
+const options: DropDownOption[] = [
+  {
+    label: "Team management",
+    linkTo: null
+  },
+  {
+    label: "Metrics",
+    linkTo: null
+  }
+];
 
 storiesOf("DropDown", module)
   .addDecorator(centered)
-  .add("Default", () => <DropDown title="Features" color="#416dea" />)
+  .add("Default", () => <DropDown title="Features" color="#416dea" options={options}/>)
