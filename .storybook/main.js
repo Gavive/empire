d400326774fe6da828543d592bca01b818b263fe
Vishrut Reddi <vishrutreddi@gmail.com>
2020-08-29 18:16:10 -0700

    Fix documentation for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,9 @@ const path = require("path");
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
   // Add any Storybook addons you want here: https://storybook.js.org/addons/
-  addons: [{
+  addons: [
+    "@storybook/addon-knobs",
+    {
     name: '@storybook/addon-essentials',
     options: {
       backgrounds: true
--- src/Tile/Type.stories.tsx
+++ src/Tile/Type.stories.tsx
@@ -2,8 +2,9 @@ import { storiesOf } from '@storybook/react';
 import centered from '@storybook/addon-centered/react';
 import styled from "styled-components";
 import React from "react";
-
 import Tile from './Tile';
+
+import { text, withKnobs } from '@storybook/addon-knobs';
 
 const Container = styled.div`
   width: 100vw;
@@ -11,15 +12,19 @@ const Container = styled.div`
   justify-content: space-between;
 `;
 
-storiesOf("Tile", module)
-  .addDecorator(centered)
-  .add("Default", () => (
-    <Container>
-      <Tile shadowType="desaturated">
-        Desaturated shadow, no negative spread
-      </Tile>
-      <Tile shadowType="saturated">
-        Saturated shadow, with negative spread
-      </Tile>
-    </Container>
-  ))
+export default {
+  component: Tile ,
+  title: 'Tile',
+  decorators: [withKnobs],
+};
+
+export const Default = () => (
+  <Container>
+    <Tile shadowType="desaturated">
+      {text('Desaturated label', 'Desaturated shadow, no negative spread')}
+    </Tile>
+    <Tile shadowType="saturated">
+      {text('Saturated label', 'Saturated shadow, with negative spread')}
+    </Tile>
+  </Container>
+);
