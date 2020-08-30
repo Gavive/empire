219b7cbf3c8ca90c484d467e194c590c3c03e0d3
Vishrut Reddi <vishrutreddi@gmail.com>
2020-08-29 18:14:35 -0700

    Fix storybook

--- src/Button/Button.stories.tsx
+++ src/Button/Button.stories.tsx
@@ -1,9 +1,21 @@
-import { storiesOf } from '@storybook/react';
+import { Story } from '@storybook/react';
 import centered from '@storybook/addon-centered/react';
 import React from "react";
+import { action } from '@storybook/addon-actions';
+import { withKnobs, text } from '@storybook/addon-knobs';
+
 
 import Button from "./Button";
+import { Props } from './Button.types';
 
-storiesOf("Button", module)
-  .addDecorator(centered)
-  .add("Default", () => <Button label="Click me" />)
+// const Template: Story<Props> = (args) => <Button {...args} />;
+
+// export const Primary = Template.bind({});
+// Primary.args = { label: 'Button' };
+export default {
+  component: Button ,
+  title: 'Button',
+  decorators: [withKnobs],
+};
+
+export const Default = () => <Button label={text('Label', 'Hello Storybook')} />;
--- src/DropDown/DropDown.stories.tsx
+++ src/DropDown/DropDown.stories.tsx
@@ -1,21 +1,21 @@
-import { storiesOf } from '@storybook/react';
-import centered from '@storybook/addon-centered/react';
-import React from "react";
+// import { storiesOf } from '@storybook/react';
+// import centered from '@storybook/addon-centered/react';
+// import React from "react";
 
-import DropDown from './DropDown';
-import { DropDownOption } from './DropDown.types';
+// import DropDown from './DropDown';
+// import { DropDownOption } from './DropDown.types';
 
-const options: DropDownOption[] = [
-  {
-    label: "Team management",
-    linkTo: null
-  },
-  {
-    label: "Metrics",
-    linkTo: null
-  }
-];
+// const options: DropDownOption[] = [
+//   {
+//     label: "Team management",
+//     linkTo: null
+//   },
+//   {
+//     label: "Metrics",
+//     linkTo: null
+//   }
+// ];
 
-storiesOf("DropDown", module)
-  .addDecorator(centered)
-  .add("Default", () => <DropDown title="Features" color="#416dea" options={options}/>)
+// storiesOf("DropDown", module)
+//   .addDecorator(centered)
+//   .add("Default", () => <DropDown title="Features" color="#416dea" options={options}/>)
